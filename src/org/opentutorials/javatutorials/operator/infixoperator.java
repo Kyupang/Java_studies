b8cc94b8aee954a3340737d8db38e77e86f04832
KimKyuHwan <vkdls754@gmail.com>
2022-08-17 16:01:36 +0900

    commit

---
+++ src/org/opentutorials/javatutorials/operator/infixoperator.java
@@ -0,0 +1,14 @@
+package org.opentutorials.javatutorials.operator;
+
+public class infixoperator {
+
+	public static void main(String[] args) {
+		int i = 3;
+		i++;
+		System.out.println(i);
+		System.out.println(++i);
+		System.out.println(i++);
+		System.out.println(i);
+	}
+
+}
